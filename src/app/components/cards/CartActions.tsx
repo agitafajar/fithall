d19d119cc755174626cc0417e0690b717fde8e1d
fakhrianaziz <fakhrianaziz@gmail.com>
2024-02-28 10:00:13 +0700

    fixing button cartaction in mobile

--- src/app/components/cards/CartActions.tsx
+++ src/app/components/cards/CartActions.tsx
@@ -31,23 +31,23 @@ const CartActions: FC<CartActionsProps> = ({ totalSubTotal, totalCart }) => {
         </Link>
       </div>
 
-      <div className="sm:flex flex-col md:hidden lg:hidden xl:hidden w-full border-t-2 fixed items-start bottom-0 left-0 p-4 bg-white z-[3000] flex gap-2 justify-end">
-        <div className="mr-4">
+      <div className="sm:flex flex-col md:hidden lg:hidden xl:hidden w-full border-t-2 fixed items-start bottom-0 left-0 px-4 pt-3 bg-white z-[3000] flex justify-end">
+        <div className="mr-0">
           <p className="text-xs">Total Amount</p>
           <p className="font-bold text-primary">
             {formatToCurrency(totalSubTotal)}
           </p>
         </div>
-        <div className="flex justify-between">
+        <div className="flex w-full justify-between">
           <Link
             href="/checkout"
-            className="cursor-pointer mr-4 my-4 border-2 border-primary py-3 md:py-2 md:my-4 text-primary bg-white px-4 rounded-md font-semibold text-sm"
+            className="w-full flex cursor-pointer mr-4 my-4 border-2 border-primary py-3 md:py-2 md:my-4 text-primary items-center justify-center text-center bg-white px-4 rounded-md font-semibold sm:text-sm xs:text-xs"
           >
             + Keranjang
           </Link>
           <Link
             href="/checkout"
-            className="cursor-pointer mr-4 my-4 lg:my-4 border-2 border-primary py-3 md:py-2 md:my-4 text-white bg-primary px-8 rounded-md font-semibold text-sm"
+            className="w-full flex cursor-pointer mr-0 my-4 lg:my-4 border-2 border-primary py-3 md:py-2 md:my-4 text-white items-center justify-center text-center bg-primary px-8 rounded-md font-semibold sm:text-sm xs:text-xs"
           >
             Checkout ({totalCart} Items)
           </Link>
