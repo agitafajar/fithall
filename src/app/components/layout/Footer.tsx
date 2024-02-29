49586450e0fe926fcb7090dee57672772546af27
agityoutap <agita.prabowo@ext.youtap.id>
2024-02-29 12:42:24 +0700

    change title

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
+
+import Link from "next/link";
+
 /* eslint-disable @next/next/no-img-element */
 export default function Footer() {
   return (
@@ -6,23 +9,37 @@ export default function Footer() {
       <div className="sm:hidden md:grid lg:grid xl:grid grid-cols-5 font-plus-jakarta-sans justify-center px-24 py-12 items-center border-b-2 mt-12 bg-primary text-white">
         <div className="col-span-2 flex flex-col gap-6 ">
           <img src="../assets/png/logo-fithall2.png" width="100px" />
-          <p className="pr-24">
-            Lörem ipsum od ohet dilogi. Bell trabel, samuligt, ohöbel utom
-            diska. Jinesade bel när feras redorade i belogi. FAR paratyp i
-            muvåning, och pesask vyfisat. Viktiga poddradio har un mad och inde.
-          </p>
+          <p className="pr-24">Fithall.id</p>
           <div className="flex gap-6">
-            <img src="../assets/svg/icon_white_facebook.svg" className="pt-1 cursor-pointer" />
-            <img src="../assets/svg/icon_white_twitter.svg" className="pt-1 cursor-pointer" />
-            <img src="../assets/svg/icon_white_linkedin.svg" className="pt-1 cursor-pointer" />
-            <img src="../assets/svg/icon_white_instagram.svg" className="pt-1 cursor-pointer" />
+            <img
+              src="../assets/svg/icon_white_facebook.svg"
+              className="pt-1 cursor-pointer"
+            />
+            <img
+              src="../assets/svg/icon_white_twitter.svg"
+              className="pt-1 cursor-pointer"
+            />
+            <img
+              src="../assets/svg/icon_white_linkedin.svg"
+              className="pt-1 cursor-pointer"
+            />
+            <img
+              src="../assets/svg/icon_white_instagram.svg"
+              className="pt-1 cursor-pointer"
+            />
           </div>
         </div>
         <div className="col-span-1 flex flex-col gap-6">
           <p className="font-bold text-lg">Pages</p>
-          <p className="cursor-pointer">Lapangan</p>
-          <p className="cursor-pointer">Kategori</p>
-          <p className="cursor-pointer">About Us</p>
+          <Link href="/lapangan" className="cursor-pointer">
+            Lapangan
+          </Link>
+          <Link href="/kategori" className="cursor-pointer">
+            Kategori
+          </Link>
+          <Link href="/about-us" className="cursor-pointer">
+            About Us
+          </Link>
         </div>
         <div className="col-span-1 flex flex-col gap-6">
           <p className="font-bold text-lg">Help & Support</p>
@@ -42,7 +59,10 @@ export default function Footer() {
           </div>
           <div className="flex gap-2 items-start cursor-pointer">
             <img src="../assets/svg/icon_white_location.svg" className="pt-1" />
-            <p>2972 Westheimer Rd. Santa Ana, Illinois 85486 </p>
+            <p>
+              Kota Jakarta Selatan Daerah Khusus Ibukota Jakarta, Indonesia
+              12760
+            </p>
           </div>
         </div>
       </div>
